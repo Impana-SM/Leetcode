7dc083a4f250ecf0fb5d84ae713aa3a117d2c7ae
Impana S M <impanasm2003@gmail.com>
2025-10-26 22:35:48 +0530

    Time: 3 ms (62.1%), Space: 12.4 MB (54.51%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        sign = 1
+        res = 0
+
+        # Skip leading whitespaces
+        while i < len(s) and s[i] == ' ':
+            i += 1
+
+        # Return 0 if only spaces are found
+        if i == len(s):
+            return 0
+
+        # Check for optional '+' or '-' sign
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        # Convert characters to integer while valid digits
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
+
+            # Clamp to 32-bit signed integer range
+            if sign * res > 2**31 - 1:
+                return 2**31 - 1
+            if sign * res < -2**31:
+                return -2**31
+
+            i += 1
+
+        # Return final result after applying sign
+        return sign * res
+        
