15b2e6b43aac9954d134720c6ca61607456b4e10
Impana S M <impanasm2003@gmail.com>
2025-11-02 18:34:45 +0530

    Time: 0 ms (100%), Space: 12.5 MB (25.85%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        ans = 1.0
+        nn = n
+        if nn < 0:
+             nn = -1 * nn
+        while nn:
+             if nn % 2:
+                 ans = ans * x
+                 nn = nn - 1
+             else:
+                 x = x * x
+                 nn = nn // 2
+        if n < 0:
+             ans = 1.0 / ans
+        return ans
+
+        
