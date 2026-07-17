a8c20076c174a35d71888ec2651088c6b0e01ac3
Impana S M <impanasm2003@gmail.com>
2026-07-17 20:36:42 +0530

    Time: 1 ms (60.16%) | Memory: 19.4 MB (29.64%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        result = []
+
+        def backtrack(index, parts, path):
+            if parts == 4:
+                if index == len(s):
+                    result.append(".".join(path))
+                return
+            for length in range(1, 4):
+                if index + length > len(s):
+                    break
+                part = s[index:index + length]
+                if len(part) > 1 and part[0] == '0':
+                    continue
+                if int(part) <= 255:
+                    path.append(part)
+                    backtrack(index + length, parts + 1, path)
+                    path.pop()
+        backtrack(0, 0, [])
+        return result
+        
